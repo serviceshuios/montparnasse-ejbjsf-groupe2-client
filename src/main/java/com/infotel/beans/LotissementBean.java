859c36c930a725335e535341c3345d7963eb9742
Benjamin Clarenc <benjamin.clarenc@gmail.com>
2019-05-15 15:25:28 +0200

    no more deprecation

--- src/main/java/com/infotel/beans/LotissementBean.java
+++ src/main/java/com/infotel/beans/LotissementBean.java
@@ -4,42 +4,44 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
 import com.infotel.ejb.ILotRemote;
 import com.infotel.metier.Lotissement;
 
-
-
-@ManagedBean
+@Named
 @SessionScoped
-public class LotissementBean implements Serializable{
-	
-	
-@EJB
-private ILotRemote service;
-private Lotissement lotissement = new Lotissement();
-private List<Lotissement> lotissements ;
-public ILotRemote getService() {
-	return service;
-}
-public void setService(ILotRemote service) {
-	this.service = service;
-}
-public Lotissement getLotissement() {
-	return lotissement;
-}
-public void setLotissement(Lotissement lotissement) {
-	this.lotissement = lotissement;
-}
-public List<Lotissement> getLotissements() {
-	return lotissements;
-}
-public void setLotissements(List<Lotissement> lotissements) {
-	this.lotissements = lotissements;
-} 
+public class LotissementBean implements Serializable {
 
+	private static final long serialVersionUID = 192071581349977345L;
+	@EJB
+	private ILotRemote service;
+	private Lotissement lotissement = new Lotissement();
+	private List<Lotissement> lotissements;
+
+	public ILotRemote getService() {
+		return service;
+	}
+
+	public void setService(ILotRemote service) {
+		this.service = service;
+	}
+
+	public Lotissement getLotissement() {
+		return lotissement;
+	}
+
+	public void setLotissement(Lotissement lotissement) {
+		this.lotissement = lotissement;
+	}
+
+	public List<Lotissement> getLotissements() {
+		return lotissements;
+	}
+
+	public void setLotissements(List<Lotissement> lotissements) {
+		this.lotissements = lotissements;
+	}
 
 }
-
--- src/main/java/com/infotel/beans/PersonneBean.java
+++ src/main/java/com/infotel/beans/PersonneBean.java
@@ -4,41 +4,44 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
 import com.infotel.ejb.ILotRemote;
 import com.infotel.metier.Personne;
 
-@ManagedBean
+@Named
 @SessionScoped
-public class PersonneBean implements Serializable{
- 
+public class PersonneBean implements Serializable {
+
+	private static final long serialVersionUID = -8967405013187651592L;
 	@EJB
 	private ILotRemote service;
 	private Personne personne = new Personne();
 	private List<Personne> personnes;
+
 	public ILotRemote getService() {
 		return service;
 	}
+
 	public void setService(ILotRemote service) {
 		this.service = service;
 	}
+
 	public Personne getPersonne() {
 		return personne;
 	}
+
 	public void setPersonne(Personne personne) {
 		this.personne = personne;
 	}
+
 	public List<Personne> getPersonnes() {
 		return personnes;
 	}
+
 	public void setPersonnes(List<Personne> personnes) {
 		this.personnes = personnes;
 	}
-
-	
-	
-	
 
 }
